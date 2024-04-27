6ec7856faddd5b241d66a273e8662c2e43be3a7e
JackCoder0 <jacksonxcontato13@gmail.com>
2024-04-26 22:36:18 -0300

    Obejtos

---
+++ 05 - Objetos/Objetos.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome: 'Jackson',
+    'nome completo': 'Jackson Antonio Silva',
+    idade: 19,
+    email: 'jacksonxcontato13@gmail.com',
+    saudacao: function () {
+        console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos de idade.`)
+    }
+};
+
+let propriedade = 'nome completo';
+
+console.log(pessoa['nome']);
+console.log(pessoa[propriedade]);
+console.log(pessoa['idade']);
+console.log(pessoa['email']);
+
+pessoa.saudacao()
