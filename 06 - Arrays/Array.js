72034a472011673ac44ee9279faaa3634bc982ed
JackCoder0 <jacksonxcontato13@gmail.com>
2024-05-01 15:20:19 -0300

    Arrays

---
+++ 06 - Arrays/Array.js
@@ -0,0 +1,10 @@
+const alunos = ['Jackson', 'João', 'Vitor', 'Giovanna', 'Mariana', 'Gojo'];
+
+console.log(alunos[5]);
+
+// Criando uma array
+let minhaArray = [10, 20, 30, 40, 50];
+
+// Acessando elementos da array
+console.log(minhaArray[0]); // Output: 10
+console.log(minhaArray[2]); // Output: 30
