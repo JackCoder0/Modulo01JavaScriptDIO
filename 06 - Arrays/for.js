624cd33d6516708ddfd4cc1bd648ae4ad1b49fa5
JackCoder0 <jacksonxcontato13@gmail.com>
2024-05-02 12:41:37 -0300

    For

---
+++ 06 - Arrays/for.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for (inicialização; condição; incremento/decremento) {
+    // bloco de código a ser repetido
+}
+
+// Exibir os números de 1 a 5
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
