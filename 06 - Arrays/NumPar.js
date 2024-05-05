144c5cd9eaadc48538a688fa2c42aa44113020d0
JackCoder0 <jacksonxcontato13@gmail.com>
2024-05-05 00:54:19 -0300

    as

--- 06 - Arrays/NumPar.js
+++ 06 - Arrays/NumPar.js
@@ -1,8 +1,8 @@
 const numeros = [5, 8, 6, 3, 4, 7, 56, 25, 3, 9];
 
 for (let i = 0; i < numeros.length; i++) {
-    const numero = numeros[i];
-    if (numero % 2 == 0) {
-        console.log(numero);
-    }
+  const numero = numeros[i];
+  if (numero % 2 == 0) {
+    console.log(numero);
+  }
 }
